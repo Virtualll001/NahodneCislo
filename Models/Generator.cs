ff6bce326e9513184b650248aec52be903de9a2b
Virtualll001 <Virtualll001@gmail.com>
2020-12-26 18:56:28 +0100

    Náhodná čísla
    Generování náhodných čísel pro Sportku.
    Čísla jsou od 1-49, neopakují se, jsou seřazená, generuje se první a druhý tah, generují se dodatková čísla.

---
+++ Models/Generator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NahodneCislo.Models
+{
+    public class Generator
+    {       
+        private Random random = new Random();
+        private List<int> seznam = new List<int>();        
+        public List<int> GenerujCislo()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                int temp = random.Next(1, 50);
+                while (seznam.Contains(temp))
+                    temp = random.Next(1, 50);
+                seznam.Add(temp);
+            }
+            seznam.Sort();
+            return seznam;
+        }
+    }
+}
